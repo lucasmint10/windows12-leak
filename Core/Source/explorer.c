10d2658d3186f0f449c1949537e670097d09cd69
Lucas Mint <117103405+lucasmint10@users.noreply.github.com>
2026-04-02 23:53:27 -0300

    Add files via upload

---
+++ Core/Source/explorer.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void){
+	system("cls");
+	int opt;
+	system("cecho.exe dark_blue black \"INFO: Do screenshot this build.\"");
+	system("cecho dark_magenta black \"--- Explorer ---\"");
+	printf("1) Command Prompt\n");
+	printf("2) System Settings\n");
+	printf("3) File Explorer\n");
+	printf("4) MS-Paint\n");
+	printf("5) Log out\n");
+	scanf("%d", &opt);
+	if(opt==1){
+		system("start cmd.exe");
+		main();
+	} else if (opt == 2) {
+		system("start sysdm.cpl");
+		main();
+	} else if (opt == 3) {
+		system("start C:/Windows/explorer.exe");
+		main();
+    } else if (opt == 4) {
+		system("start mspaint.exe");
+		main();
+	} else if (opt == 5) {
+		system("welcome.exe");
+		main();
+	} else {
+		main();
+	}
+}
---
+++ Core/Source/welcome.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+int main(void){
+    system("cls");
+	printf("--- Welcome to Windows 12 ---\n");
+	Sleep(1000);
+	system("cecho dark_cyan black \"            Soft\"");
+	printf("           Welcome\n");
+	Sleep(1000);
+	system("explorer.exe");
+}
